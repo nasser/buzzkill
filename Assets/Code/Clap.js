c48d7983b760633caeae4f1b8a4976564c71b998
Ramsey Nasser <ram@nas.sr>
2015-02-17 01:39:04 -0500

    Move debounce logic to Unity

--- Assets/Code/Clap.js
+++ Assets/Code/Clap.js
@@ -4,26 +4,37 @@ var rightHand : Transform;
 var normalHandDistance : float = 2.5;
 var clapHandDistance : float = 0.5;
 
+var minimumClapLength : float = 1.0;
+var timeSinceClapStarted : float = 0.0;
+
 function Start() {
   leftHand.position.x = -normalHandDistance;
   rightHand.position.x = normalHandDistance;
 }
 
 function Update () {
-  if(Input.GetKeyDown("space")) {
-    var flies = GameObject.FindGameObjectsWithTag("fly");
-    for(var fly in flies) {
-      fly.SendMessage("TryToSquash");
+  if(Input.GetKey("space")) {
+    timeSinceClapStarted += Time.deltaTime;
+    
+    if(timeSinceClapStarted > minimumClapLength) {
+      leftHand.position.x = -clapHandDistance;
+      rightHand.position.x = clapHandDistance;
     }
-    
-    audio.Play();
-    
-    leftHand.position.x = -clapHandDistance;
-    rightHand.position.x = clapHandDistance;
   }
   
   if(Input.GetKeyUp("space")) {
-    leftHand.position.x = -normalHandDistance;
-    rightHand.position.x = normalHandDistance;
+    if(timeSinceClapStarted > minimumClapLength) {
+      var flies = GameObject.FindGameObjectsWithTag("fly");
+      for(var fly in flies) {
+        fly.SendMessage("TryToSquash");
+      }
+      
+      audio.Play();
+      
+      leftHand.position.x = -normalHandDistance;
+      rightHand.position.x = normalHandDistance;
+    }
+    
+    timeSinceClapStarted = 0;
   }
 }
